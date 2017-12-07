ff835bfa26f0e2cdabc32c87bce8b60c17f05fa8
walkcode <walk_code@163.com>
2017-12-07 22:26:51 +0800

    test docx to pdf in centos6.9

--- app/Http/Controllers/Lemon/LemonController.php
+++ app/Http/Controllers/Lemon/LemonController.php
@@ -106,13 +106,12 @@ class LemonController extends Controller
             $file = substr($filename, 0, -5);
             $templateProcessor->saveAs($file . ".docx");
             //输出
-//            exec("libreoffice --headless --convert-to pdf ./". $file . ".docx --outdir /home/www/EX/public/PDF/",$outPut,$result);
-            //exec("libreoffice --headless --convert-to pdf ./". $file . ".docx --outdir /home/www/EX/public/PDF/",$outPut,$result);
+            exec("libreoffice --headless --convert-to pdf ./". $file . ".docx --outdir /home/www/EX/public/PDF/",$outPut,$result);exec("libreoffice --headless --convert-to pdf ./". $file . ".docx --outdir /home/www/EX/public/PDF/",$outPut,$result);
             //return exec("php -v");
             //return json_encode($outPut)."  ".json_encode($result);
             $pdf = $file . ".pdf";
-            header("Content-Disposition: attachment; filename='".$filename."'");
-            readfile($file.".docx");
+           /* header("Content-Disposition: attachment; filename='".$filename."'");
+            readfile($file.".docx");*/
             unlink($file . ".docx");
 
         } else {
